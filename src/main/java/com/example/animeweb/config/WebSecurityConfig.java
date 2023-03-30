a9f4f76e05718b66d50747f31a7f3b6f15ca42e2
Andrey Sharipov <andrey.sharipov1@gmail.com>
2023-03-30 13:24:46 +0300

    Delete any player

--- src/main/java/com/example/animeweb/config/WebSecurityConfig.java
+++ src/main/java/com/example/animeweb/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/anime", "/anime/{title}-{id}","/images/**", "/registration","file:///E:/**").permitAll()
+                        .requestMatchers("/anime", "/anime/{title}-{id}","/images/**", "/registration").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
